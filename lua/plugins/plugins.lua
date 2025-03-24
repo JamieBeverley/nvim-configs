66c7c9ffcf2b61c3d03a8eb637a5853cc8593511
Jamie Beverley <11593376+JamieBeverley@users.noreply.github.com>
2025-03-24 17:43:44 -0400

    wip

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,22 @@
+
+
+return {
+	{ "folke/which-key.nvim" },
+	{ "sharkdp/fd" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+	build = function()
+		require("nvim-treesitter.install").update({ with_syinc = true})()
+	end,
+	opts = {
+		ensure_installed = { "javascript", "python", "typescript", "html", "css", "lua" },
+		highlight = { enable = true },
+		indent = { enable = true },
+	}
+},
+{ 
+	"nvim-telescope/telescope.nvim",
+	tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter', 'sharkdp/fd' }
+}
+}

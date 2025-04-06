87d8b85681d05c5e6e812fe803b43fdb671d3b86
Jamie Beverley <11593376+JamieBeverley@users.noreply.github.com>
2025-04-06 16:52:10 -0400

    formatting

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -74,40 +74,42 @@ return {
                 config_files = { ".nvim.lua"}
             }
         },
-        { "nvimtools/none-ls.nvim", 
-        dependencies = { "nvim-lua/plenary.nvim" }
-    },
-    { "neovim/nvim-lspconfig" }, 
-    { "folke/which-key.nvim" },
-    { "sharkdp/fd" },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = function()
-            require("nvim-treesitter.install").update({ with_syinc = true})()
-        end,
-        opts = {
-            ensure_installed = { "javascript", "python", "typescript", "html", "css", "lua" },
-            highlight = { enable = true },
-            indent = { enable = true },
-        }
-    },
-    { 
-        "nvim-telescope/telescope.nvim",
-        tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter', 'sharkdp/fd' }
-    },
-    {
-        "nvim-tree/nvim-tree.lua",
-        lazy = false,
-        config = function()
-            require("nvim-tree").setup{
-                renderer = { icons = { show = { file = false, folder = false } } }
+        {
+            "nvimtools/none-ls.nvim", 
+            dependencies = { "nvim-lua/plenary.nvim" 
+        },
+        { "neovim/nvim-lspconfig" }, 
+        { "folke/which-key.nvim" },
+        { "sharkdp/fd" },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = function()
+                require("nvim-treesitter.install").update({ with_syinc = true})()
+            end,
+            opts = {
+                ensure_installed = { "javascript", "python", "typescript", "html", "css", "lua" },
+                highlight = { enable = true },
+                indent = { enable = true },
             }
-            vim.api.nvim_create_autocmd({ "VimEnter" }, {
-                callback = function()
-                    require("nvim-tree.api").tree.open()
-                end
-            })
-        end,
+        },
+        { 
+            "nvim-telescope/telescope.nvim",
+            tag = '0.1.8',
+            dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter', 'sharkdp/fd' }
+        },
+        {
+            "nvim-tree/nvim-tree.lua",
+            lazy = false,
+            config = function()
+                require("nvim-tree").setup{
+                    renderer = { icons = { show = { file = false, folder = false } } }
+                }
+                vim.api.nvim_create_autocmd({ "VimEnter" }, {
+                    callback = function()
+                        require("nvim-tree.api").tree.open()
+                    end
+                })
+            end,
+        }
     }
-}
+

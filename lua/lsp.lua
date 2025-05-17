06f4affc4cc0106884b4063db4c4df6870de2518
Jamie Beverley <11593376+JamieBeverley@users.noreply.github.com>
2025-05-17 08:54:14 -0400

    fix pyright for non-python repos

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -14,22 +14,33 @@ local util = require('lspconfig.util')
 local root_pattern = util.root_pattern('pyrightconfig.json', 'pyproject.toml', '.git')
 
 lspconfig.pyright.setup({
-  root_dir = root_pattern,
-  settings = (function()
-    local root = root_pattern(vim.fn.getcwd())
-    local pyright_config = root and root .. '/pyrightconfig.json'
-    local has_pyright_config = pyright_config and vim.fn.filereadable(pyright_config) == 1
-    if has_pyright_config then
-      return {} -- Don't override; let pyrightconfig.json handle it
-    else
-      return {
-        python = {
-          venvPath = root,
-          venv = root .. ".venv",
-        }
-      }
-    end
-  end)()
+    root_dir = root_pattern,
+    settings = (function()
+        local root = root_pattern(vim.fn.getcwd())
+        if not root then
+            return {}
+        end
+
+        local pyright_config = root and root .. '/pyrightconfig.json'
+        local has_pyright_config = pyright_config and vim.fn.filereadable(pyright_config) == 1
+        if has_pyright_config then
+            return {} -- Don't override; let pyrightconfig.json handle it
+        else
+            -- if root/.venv exists then use it, otherwise leave unconfigured
+            local venv = root .. '.venv'
+            local has_venv = venv and vim.fn.isdirectory(venv) == 1
+            if has_venv then
+                return {
+                    python = {
+                        venvPath = root,
+                        venv = root .. '.venv'
+                    }
+                }
+            end
+            return {
+            }
+        end
+    end)()
 })
 
 lspconfig.ts_ls.setup{}

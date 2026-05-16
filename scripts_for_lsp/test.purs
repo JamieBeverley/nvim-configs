5d0105bb05af8da43e06c0b07eb24e8aada00890
Jamie Beverley <11593376+JamieBeverley@users.noreply.github.com>
2026-05-16 08:58:07 -0400

    scripts for testing lsp

---
+++ scripts_for_lsp/test.purs
@@ -0,0 +1,11 @@
+module Main where
+
+import Prelude
+import Effect (Effect)
+import Effect.Console (log)
+
+greet :: String -> String
+greet name = "Hello, " <> name
+
+main :: Effect Unit
+main = log (greet "world")
---
+++ scripts_for_lsp/test.ts
@@ -0,0 +1,7 @@
+function greet(name: string): string {
+    return `Hello, ${name}`;
+}
+
+const result = greet("world");
+console.log(result);
+
